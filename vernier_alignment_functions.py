aaaa8d14b196fd4483a7335486b91d928eaff9ae
Tom Smart <tomjsmart@hotmail.co.uk>
2020-04-04 10:30:03 +0100

    tidying up

--- vernier_alignment_functions.py
+++ vernier_alignment_functions.py
@@ -7,15 +7,14 @@ Created on Mon Dec  2 11:03:41 2019
 
 import numpy as np
 import pandas as pd
-from psychopy import monitors, visual
+from psychopy import monitors, visual, event
 import tkinter as tk
 from tkinter import filedialog
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-import config
-#import pdb
-import traceback
+import config, traceback, collections, json, random
 from tabulate import tabulate
+
 
 
 def reflection_matrix():
@@ -30,10 +29,12 @@ def rotation_matrix(theta):
     return rotation_mat
 
 
-def initialise_shapes(shape_parameters, run_type = 'trial'):
+def initialise_shapes(shape_parameters, run_type = 'test'):
+    
+    """ """
     
     if run_type == 'test':
-        win = visual.Window(screen = 2,
+        win = visual.Window(screen = 0,
                             color = shape_parameters['background_color'])
     
         """ Create lines to be presented on screens. Line vertices aranged here 
@@ -44,8 +45,8 @@ def initialise_shapes(shape_parameters, run_type = 'trial'):
         shape_position =  np.array([shape_parameters['line_length']/2 + shape_parameters['line_displacement'], shape_parameters['screen_position']])
         
         """ add values to shape parameters dictionary """
-        shape_parameters['vertices'] = shape_vertices
-        shape_parameters['position'] = shape_position
+        shape_parameters['vertices'] = shape_vertices.tolist()
+        shape_parameters['position'] = shape_position.tolist()
             
         """ red line """
         shape_red = visual.ShapeStim(win, 
@@ -67,6 +68,7 @@ def initialise_shapes(shape_parameters, run_type = 'trial'):
     
     
     else:
+        
         """ One set of coordinates is used for both lines, but the position of 
         line 2 is the relfection in the y-axis. Screen 2 is seen as a reflection #
         of itself, due to the reflection at the beam splitter. To counteract this, 
@@ -127,23 +129,33 @@ def initialise_shapes(shape_parameters, run_type = 'trial'):
         return wins, shape1, shape2, shape_parameters
 
 
-def update_shapes(shape1, shape2, shape_parameters, orientation = 0, offset = 0, run_type = 'trial'):
+def update_shapes(shape1, shape2, shape_parameters, orientation = 0, offset = 0, run_type = 'test'):
+    
+    """ """
+    
+    
     rotate_mat = rotation_matrix(np.radians(orientation))
     reflect_mat = config.reflect_mat
     
+    position = np.array(shape_parameters['position'])
+    vertices = np.array(shape_parameters['vertices'])
+    correction = np.array(shape_parameters['correction'])
+    
     if run_type == 'test':
-        shape1.pos = rotate_mat.dot(shape_parameters['position']).transpose()
-        shape1.vertices = rotate_mat.dot(shape_parameters['vertices']).transpose()
-        shape2.pos = rotate_mat.dot(shape_parameters['position'] + np.array([0, offset])).transpose() + shape_parameters['correction']
-        shape2.vertices = rotate_mat.dot(shape_parameters['vertices']).transpose()
+        
+        shape1.pos = rotate_mat.dot(position).transpose()
+        shape1.vertices = rotate_mat.dot(vertices).transpose()
+        
+        shape2.pos = rotate_mat.dot((np.array([-1, 1]) * position + np.array([0, offset]))).transpose()
+        shape2.vertices = rotate_mat.dot(vertices).transpose()
         
     else:
-        shape1.pos = rotate_mat.dot(shape_parameters['position']).transpose()
-        shape1.vertices = rotate_mat.dot(shape_parameters['vertices']).transpose()
-        shape2_shift = shape_parameters['position'] + np.array([0, offset])
-        shape2.pos = reflect_mat.dot(rotate_mat.dot(reflect_mat.dot(shape2_shift))).transpose() + shape_parameters['correction']
-#        shape2.pos = rotate_mat.dot(shape2_shift).transpose()
-        shape2.vertices = rotate_mat.dot(shape_parameters['vertices']).transpose()
+        shape1.pos = rotate_mat.dot(position).transpose()
+        shape1.vertices = rotate_mat.dot(vertices).transpose()
+
+        shape2_shift = position + correction + np.array([0, offset])
+        shape2.pos = reflect_mat.dot(rotate_mat.dot(reflect_mat.dot(shape2_shift))).transpose()
+        shape2.vertices = rotate_mat.dot(vertices).transpose()
     
     return shape1, shape2
 
@@ -224,4 +236,112 @@ def tabulate_results(orientations, fit_results):
         table.append([orientation, fit_results[orientation]['popt'][0]])
     print(tabulate(table, headers = ["Orientation", "Offset"]))
     return table
+
+
+""" Trial class """
+vernier_trial = collections.namedtuple('trial', ['orientation','offset'])
+
+
+class vernier_trial_list:
+
+    """ Trial List class """
+    def __init__(self, orientations = np.array([0,90,180,270]), offsets = np.arange(-10,12,2), nreps = 1):
+        self.orientations = orientations
+        self.offsets = offsets
+        self._trials = [vernier_trial(orientation, offset) 
+                        for orientation in self.orientations 
+                        for offset in self.offsets]*nreps
+        
+    def __len__(self):
+        return len(self._trials)
     
+    def __getitem__(self, position):
+        return self._trials[position]
+    
+    def __setitem__(self, position, trial):
+        self._trials[position] = trial
+        
+    def sort_by_offset(self):
+        self._trials = sorted(self, key = lambda trial : trial.offset)
+        
+    def shuffle_trials(self):
+        self.sort_by_offset()
+        new_list = []
+        new_trial_list = []
+        self.sort_by_offset()
+        for trial in self:
+            dups = np.nonzero(new_list == trial.orientation)
+            dups = np.concatenate([dups[0], dups[0] + 1])
+            non_dups = np.setdiff1d(np.arange(0, len(new_list)+1), dups)
+            new_pos = non_dups[random.randint(0, len(non_dups)-1)]
+            new_list = np.insert(new_list, new_pos, trial.orientation)
+            new_trial_list.insert(new_pos, vernier_trial(trial.orientation, trial.offset))
+        self._trials = new_trial_list
+    
+
+
+""" Test functions """
+
+if __name__ == '__main__':
+
+    run_type = 'test'
+
+    """ Shape (line) parameters """
+    with open("default_shape_parameters.json","r") as read_file:
+        shape_parameters = json.load(read_file)
+
+
+    """ Trial parameters """ 
+    with open("default_trial_parameters.json","r") as read_file:
+        trial_parameters = json.load(read_file)
+
+
+    """ Build trial list class - # uses purpose built 'vernier_trial_list' class"""
+    trial_list = vernier_trial_list(trial_parameters['orientations'],
+                                                trial_parameters['offsets'],
+                                                trial_parameters['nreps'],
+                                                )
+    
+    trial_list.shuffle_trials() 
+
+
+    """ Initialise monitors, windows and shapes. A try/except/else structures 
+    should prevent a window being presented if there is an error in set-up (aiming
+    to avoid kernel death!) """
+    try:
+        wins, line1, line2, shape_parameters = initialise_shapes(shape_parameters, 
+                                                                 run_type)
+
+    except Exception:
+        traceback.print_exc()
+
+    else:
+        
+        """ Trial starts here """
+        try:
+            for trial in trial_list:
+                line1, line2 = update_shapes(line1, 
+                                             line2, 
+                                             shape_parameters, 
+                                             trial.orientation, 
+                                             trial.offset, 
+                                             run_type)
+                line1.draw()
+                line2.draw()
+                [win.flip() for win in wins]
+                
+                key_choice = event.waitKeys()                
+
+                
+                if key_choice[0] == 'escape':
+                    break
+                
+        
+            """ End trial """
+            [win.close() for win in wins]
+
+        
+        except Exception:
+            """ Close windows """
+            [win.close() for win in wins]
+            traceback.print_exc()
